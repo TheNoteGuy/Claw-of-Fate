73210613de7f603740af59d232f837530dd16cd8
Lucy <30p87@30p87.de>
2026-07-18 11:34:35 +0200

    apply .gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_yourteam_cardgacharpg_App.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.yourteam.cardgacharpg.App"
-)
-public final class _com_yourteam_cardgacharpg_App {
-}
